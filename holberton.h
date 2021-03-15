f41e024f9ba080be969a908f0f45d3d794c18a13
Simon Sun <sunmingyu.97@gmail.com>
2021-03-15 17:39:16 +0000

    split the main file to smaller function c file

--- holberton.h
+++ holberton.h
@@ -1,11 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdarg.h>
-static int count;
+extern static int count;
 int _putchar(char c);
 int _printf(const char *format, ...);
 int func_S(char *, char *);
 int func_S(char *, char *);
+void func_c(int a, char *p);
+int func_s(char *c, char *p);
+int func_int(int a, char *p);
+int func_bin(unsigned int a, char *p);
 
 
 
